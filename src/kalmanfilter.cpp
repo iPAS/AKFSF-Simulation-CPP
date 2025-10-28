22041dc90e75d55be382b630203feae88f87f2aa
iPAS <ptiwatthanont@gmail.com>
2025-10-28 13:26:50 +0700

    feat: wait for GPS data as initial state

--- src/kalmanfilter.cpp
+++ src/kalmanfilter.cpp
@@ -11,7 +11,7 @@
 
 // -------------------------------------------------- //
 // YOU CAN USE AND MODIFY THESE CONSTANTS HERE
-constexpr bool INIT_ON_FIRST_PREDICTION = true;
+constexpr bool INIT_ON_FIRST_PREDICTION = false;  // true
 constexpr double INIT_POS_STD = 0;  // 5
 constexpr double INIT_VEL_STD = 0;  // 5./3.
 constexpr double ACCEL_STD = 0.1;  // .1
